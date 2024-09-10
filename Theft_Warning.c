192202fa484441355ccbad53076f39a8a2cfafcc
Thanveertt <thanveer7tt@gmail.com>
2024-09-10 21:04:18 +0530

    Create Theft_Warning.c
    Initialisation

---
+++ Theft_Warning.c
@@ -0,0 +1,38 @@
+#include <SoftwareSerial.h>
+SoftwareSerial m(2, 3);    //rx 2 tx 3
+volatile int call_stat=0;
+volatile int message_stat=0;
+void setup() 
+{
+  // put your setup code here, to run once:
+m.begin(9600);//gsm
+Serial.begin(9600);
+pinMode(5,INPUT);
+pinMode(LED_BUILTIN, OUTPUT);
+pinMode(6, OUTPUT);
+m.println("AT");
+delay(2000);
+}
+
+void loop() 
+{
+  if(digitalRead(5))
+  {
+      digitalWrite(LED_BUILTIN, HIGH );
+      delay(1000);
+      digitalWrite(5, HIGH );
+      delay(1000);
+      if (call_stat==0)
+      {
+      call();
+      }
+      if (message_stat==0)
+      {
+      message();
+      }
+      }
+      digitalWrite(LED_BUILTIN, LOW );
+      delay(1000);
+      digitalWrite(6, LOW );
+      delay(1000);
+}
